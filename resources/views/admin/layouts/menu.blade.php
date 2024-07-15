4cf1de129fa192aea1bc8f65539f0b9e311b806e
Khanh <Khanh021101109Wtgu.edu.vn>
2024-07-15 09:23:59 +0700

    icons new

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -15,10 +15,7 @@
               <img src="{{ asset('assets/images/logout.png') }}" width="20px" style="background: rgb(128, 126, 132)" alt="Logout">
               <span style="font-size: 10px">Đăng xuất</span>
           </button>
-        
-          
       </form> --}}
-      
       
         {{-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a> --}}
       </div>
@@ -39,7 +36,7 @@
     <ul class="sidebar-menu" data-widget="tree">
       <li>
         <a href="{{route('admin.index')}}">
-          <i class="fa fa-th"></i> <span>Trang chủ</span>
+          <i class="fa fa-home" style="font-size: 20px"></i> <span>Trang chủ</span>
           <span class="pull-right-container">
          
           </span>
@@ -48,35 +45,31 @@
       </li>
       <li class="treeview">
         <a href="#">
-          <i class="fa fa-dashboard"></i> <span>Quản lý Nhập hàng </span>
+          <i class="fa fa-download" style="font-size: 20px"></i> <span>Quản lý Nhập hàng </span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          {{-- <li><a href="{{route('kho.index')}}"><i class="fa fa-circle-o"></i> Danh sách mặt hàng </a></li> --}}
-          <li><a href="{{route('nhap.index')}}"><i class="fa fa-circle-o"></i> Nhập hàng </a></li>
-          <li><a href="{{route('nhap.list')}}"><i class="fa fa-circle-o"></i> Sản phẩm đã nhập </a></li>
+          <li><a href="{{route('nhap.index')}}"><i class="fa fa-cart-plus" style="font-size: 15px"></i> Nhập hàng </a></li>
+          <li><a href="{{route('nhap.list')}}"><i class="fa fa-gift" style="font-size: 15px"></i> Sản phẩm đã nhập </a></li>
         </ul>
       </li>
       <li class="treeview">
         <a href="#">
-          <i class="fa fa-dashboard"></i> <span>Quản lý Xuất hàng </span>
+          <i class="fa fa-truck" style="font-size: 20px"></i> <span>Quản lý Xuất hàng </span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          {{-- {{route('xuat.index')}}  {{route('xuat.list')}} --}}
-
-          {{-- <li><a href="{{route('kho.index')}}"><i class="fa fa-circle-o"></i> Danh sách mặt hàng </a></li> --}}
-          <li><a href="{{route('xuat.index')}}"><i class="fa fa-circle-o"></i> Xuất hàng </a></li>
-          <li><a href="{{route('xuat.list')}}"><i class="fa fa-circle-o"></i> Sản phẩm đã xuất </a></li>
+          <li><a href="{{route('xuat.index')}}"><i class="fa fa-send" style="font-size: 15px"></i> Xuất hàng </a></li>
+          <li><a href="{{route('xuat.list')}}"><i class="fa fa-gift" style="font-size: 15px"></i> Sản phẩm đã xuất </a></li>
         </ul>
       </li>
       <li>
         <a href="{{route('khohang.index')}}">
-          <i class="fa fa-th"></i> <span>Kho hàng</span>
+          <i class="fa fa-archive" style="font-size: 20px"></i> <span>Kho hàng</span>
           <span class="pull-right-container">
          
           </span>
@@ -95,7 +88,7 @@
       
       <li>
         <a href="{{route('category.index')}}">
-          <i class="fa fa-th"></i> <span>Quản lý danh mục </span>
+          <i class="fa fa-navicon" style="font-size: 20px"></i> <span>Quản lý danh mục </span>
           <span class="pull-right-container">
          
           </span>
@@ -104,7 +97,7 @@
       </li>
       <li>
         <a href="{{route('thongke.doanhthu')}}">
-          <i class="fa fa-th"></i> <span>Thống kê </span>
+          <i class="fa fa-pie-chart" style="font-size: 20px"></i> <span>Thống kê </span>
           <span class="pull-right-container">
          
           </span>
@@ -114,14 +107,14 @@
 
       <li class="treeview">
         <a href="#">
-          <i class="fa fa-dashboard"></i> <span>Quản lý sản phẩm </span>
+          <i class="fa fa-th" style="font-size: 20px"></i> <span>Quản lý sản phẩm </span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="{{route('product.index')}}"><i class="fa fa-circle-o"></i> Danh sách sản phẩm </a></li>
-          <li><a href="{{route('product.create')}}"><i class="fa fa-circle-o"></i> Thêm mới sản phẩm </a></li>
+          <li><a href="{{route('product.create')}}"><i class="fa fa-plus-square" style="font-size: 15px"></i> Thêm mới sản phẩm </a></li>
+          <li><a href="{{route('product.index')}}"><i class="fa fa-cubes" style="font-size: 15px"></i> Danh sách sản phẩm </a></li>
         </ul>
       </li>
       <!-- <li>
